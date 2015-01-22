1da99c5ece464e39cdf367716e4d053dd1ab52fa
Mark Thomas <markthethomas@gmail.com>
2015-01-21 21:16:54 -0800

    Scaffold connection to test db

--- server/models/recipes.js
+++ server/models/recipes.js
@@ -1,50 +1,65 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/test');
 
-var recipeSchema = new Schema({
-  author: String,
-  accommodations: Array,
-  cost: [{totalCost: Number, plainEnglishTotalCost: String}],
-  dateCreated: { type: Date, default: Date.now },
-  generalType: Array,
-  ingredients: [{
-    ingredientName: String,
-    ingredientAmount: [{
-      metric: Boolean,
-      metricAmount: {
-        amount: Number,
-        unit: String
-      },
-      imeprial: Boolean,
-      imperialAmount: {
-        amount: Number,
-        unit: String
-      },
-      cost: Number
-    }]
-  }],
-  image: {
-    imageURI: String,
-    sizes: [{dimension: String, uri: String}]
-  },
-  mealOfDayType: String,
-  meta: {
-    likes: Number,
-    favorites: Number,
-    categories: [{name: String}],
-    keywords: Array
-  },
-  rating: Number,
-  season: String,
-  share: {
-    publicURI: String
-  },
-  servings: Number,
-  steps: [{stepIndex: Number, stepDescription: String}],
-  summary: String,
-  tags: Array,
-  totalPrepTime: [{active: Number, inactive: Number}],
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+
+  var Schema = mongoose.Schema;
+
+  var recipeSchema = new Schema({
+    cost: [{totalCost: Number, plainEnglishTotalCost: String}],
+    ingredients: [{
+      ingredientName: String,
+      ingredientAmount: [{
+        metric: Boolean,
+        metricAmount: {
+          amount: Number,
+          unit: String
+        },
+        imeprial: Boolean,
+        imperialAmount: {
+          amount: Number,
+          unit: String
+        },
+        cost: Number
+      }]
+    }],
+    image: {
+      imageURI: String,
+      sizes: [{dimension: String, uri: String}]
+    },
+    meal: {
+      mealOfDayType: String,
+      cuisineType: Array,
+      season: String,
+    },
+    meta: {
+      accommodations: [{type: String, notes: String}],
+      author: String,
+      categories: [{name: String}],
+      dateCreated: { type: Date, default: Date.now },
+      favorites: Number,
+      keywords: Array,
+      likes: Number,
+      rating: Number,
+      summary: String,
+      tags: Array,
+    },
+    yield: {
+      servings: Number, default: 1,
+      shelfLife: {
+        refrigerate: Boolean,
+        lifespan: Number
+      }
+    },
+    share: {
+      publicURI: String
+    },
+    steps: [{stepIndex: Number, stepDescription: String}],
+    totalPrepTime: [{active: Number, inactive: Number}],
+  });
+
+
+  module.exports = mongoose.model('Recipe', recipeSchema);
 });
-
-
-var Recipe = mongoose.model('Recipe', recipeSchema);
